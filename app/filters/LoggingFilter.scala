5c30807156c853ad1ada067b2354d731bcaadedf
Markus Paasovaara <markus.paasovaara@futurice.com>
2019-01-20 15:23:36 +0200

    Added logging filter

---
+++ app/filters/LoggingFilter.scala
@@ -0,0 +1,25 @@
+package filters
+
+import javax.inject.Inject
+import akka.stream.Materializer
+import play.api.Logger
+import play.api.mvc._
+import utils.Log
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class LoggingFilter @Inject() (implicit val mat: Materializer, ec: ExecutionContext) extends Filter with Log {
+
+  def apply(nextFilter: RequestHeader => Future[Result])
+           (requestHeader: RequestHeader): Future[Result] = {
+
+    val startTime = System.currentTimeMillis
+    nextFilter(requestHeader).map { result =>
+      val endTime = System.currentTimeMillis
+      val requestTime = endTime - startTime
+
+      info(s"${requestHeader.method} ${requestHeader.uri} took ${requestTime}ms and returned ${result.header.status}")
+      result.withHeaders("Request-Time" -> requestTime.toString)
+    }
+  }
+}
